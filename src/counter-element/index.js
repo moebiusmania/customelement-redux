23a2b1cc30e2d3f5fc21f05ebd2b15cd02b650a4
Salvatore Laisa <moebiusmania@gmail.com>
2017-11-04 21:32:58 +0100

    fixed stateless element to have attributes and properties reflection

---
+++ src/counter-element/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+export class CounterElement extends HTMLElement {
+
+  static get observedAttributes() {
+    return ['value'];
+  }  
+
+  constructor(){
+    super();
+
+    this.shadow = this.attachShadow({mode: 'open'});
+    this.state = {
+      value: 0
+    }
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    const condition = this.state[name] !== newValue;
+    condition ? this.state[name] = newValue : null;
+    this.render();
+  }
+
+  render(){
+    this.shadow.innerHTML = `
+      <style>
+        section{
+          border: 1px solid #ddd;
+          padding: 1px 7px;
+          margin: 15px 0;
+          font-family: Helvetica, Arial, sans;
+        }
+      </style>
+      <section>
+         <p>Total: <strong>${this.state.value}</strong></p>
+      </section>
+    `;
+  }
+
+  _update(obj, prop, value){
+    obj[prop] = value;
+    this.setAttribute('value', value);
+    this.render();
+    return true;
+  }
+
+  connectedCallback(){
+    console.log('<counter-element> INIT');
+
+    this.state = new Proxy({
+      value: this.getAttribute('value') || 0
+    }, { set: this._update.bind(this) });
+  }  
+
+}
+
+customElements.define('counter-element', CounterElement);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './src/demo.js'
+    ],
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'demo.js',
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        watchContentBase: true,
+        watchOptions: {
+            poll: true
+        },
+        compress: true,
+        port: 8081,
+        host: 'localhost',
+        hot: true,
+        inline: true
+    }
+}
